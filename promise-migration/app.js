cca10dd7b766000a6e47dff29e9e24cb00f5af86
github_d18mm <ilham18mm@gmail.com>
2017-09-08 10:27:21 +0700

    promise-migration

--- promise-migration/app.js
+++ promise-migration/app.js
@@ -8,6 +8,22 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
+app.get('/firs_name', function (req, res) {
+  //res.send('Hello World!')
+  res.render('firs_name')
+})
+
+app.get('/last_name', function (req, res) {
+  //res.send('Hello World!')
+  res.render('last_name')
+
+})
+
+app.get('/fuul_name', function (req, res) {
+  //res.send('Hello World!')
+  res.render('fuul_name')
+})
+
 
 app.listen(3018, function () {
   console.log(' star 3018!')
