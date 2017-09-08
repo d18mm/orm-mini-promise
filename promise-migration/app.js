d55c5bd3f27ba0bee09daca8de20390ac4bb9b85
github_d18mm <ilham18mm@gmail.com>
2017-09-08 11:43:57 +0700

    hanya promise

--- promise-migration/app.js
+++ promise-migration/app.js
@@ -1,27 +1,34 @@
 const express = require('express')
 const app = express()
 var sqlite3   = require('sqlite3').verbose();
+// var db = new sqlite3.Database('db/database.db');
+
 
 app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
   //res.send('Hello World!')
   res.render('index')
 })
+//
+app.get('/siswa', function (req, res) {
+  db.all(`SELECT * FROM siswa this=`,(err,rows)=>{res.render('siswa',{data:rows})
 
-app.get('/firs_name', function (req, res) {
-  //res.send('Hello World!')
-  res.render('firs_name')
+  })
 })
+//   db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+// //rows contain values while errors, well you can figure out.
+// });
+
 
 app.get('/last_name', function (req, res) {
-  //res.send('Hello World!')
+
   res.render('last_name')
 
 })
 
-app.get('/fuul_name', function (req, res) {
-  //res.send('Hello World!')
-  res.render('fuul_name')
+app.get('/firs_name', function (req, res) {
+
+  res.render('firs_name')
 })
 
 
